7ef1368e1273e82979c2e367671e1114ba20129b
Aryan Verma <aryan.verma@adypu.edu.in>
2026-03-17 16:27:35 +0530

    Mapped Hands

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    max_num_hands=2,
+    min_detection_confidence=0.7,
+    min_tracking_confidence=0.7
+)
+
+mp_draw = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
+
+cap.set(3, 640) 
+cap.set(4, 480) 
+
+frame_count = 0
+
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    cv2.imshow("Camera Test", frame)
+    frame = cv2.flip(frame, 1)
+
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    rgb_frame.flags.writeable = False
+    results = hands.process(rgb_frame)
+    rgb_frame.flags.writeable = True
+
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame,hand_landmarks, mp_hands.HAND_CONNECTIONS )
+
+    cv2.imshow("Hand Detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
